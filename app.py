dd984fbc5a4915742d9d15d5af72e4499a8cb157
Max1-618 <contactmayank.pathak@gmail.com>
2025-04-08 13:37:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,7 @@ def fetch_les_echos_news():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
-    chromedriver_path = r"C:\Program Files\Google\Chrome\CHROME DRIVER\chromedriver-win64\chromedriver.exe"
-    service = Service(chromedriver_path)
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get("https://www.lesechos.fr/finance-marches")
     news = []
     headlines = driver.find_elements(By.TAG_NAME, "h3")
