1a9d3a22a33da25eb445ff331843d681667e234c
Max1-618 <contactmayank.pathak@gmail.com>
2025-06-26 00:44:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,7 @@ def home():
     return render_template('index.html', all_news=all_news)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
